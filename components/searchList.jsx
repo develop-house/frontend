8727332eae61081635c0b7a0430d6db2e01dec75
Kwonkunkun <kunwoo5012@naver.com>
2021-02-26 00:45:02 +0900

    💄add search ui

---
+++ components/searchList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const SearchList = () => {
+  return <ul></ul>;
+};
+
+export default SearchList;
---
+++ pages/search.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchForm from '../components/searchForm';
+import SearchList from '../components/searchList';
+
+const Search = () => {
+  return (
+    <>
+      <SearchForm />
+      <SearchList />
+    </>
+  );
+};
+
+export default Search;
